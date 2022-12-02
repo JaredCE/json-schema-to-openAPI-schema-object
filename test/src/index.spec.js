86602121e0d2dca3335068926e42d64d0a4e1a0c
JaredAAT <jarede@gmail.com>
2022-12-02 19:33:09 +0000

    simplify these tests

--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -4,29 +4,17 @@ const expect = require('chai').expect
 
 const Factory = require('../../index')
 
-const simpleSchema = require('../schemas/simple-one')
-const complexAllOfSchema = require('../schemas/complex-allOf')
+const basic = require('../schemas/basic.json')
 
 describe('index', () => {
-    beforeEach(function() {
-        delete require.cache[require.resolve('../schemas/simple-one')];
-        delete require.cache[require.resolve('../schemas/complex-allOf')];
-    });
-
     describe('convert', () => {
         it('should produce a converted schema', function() {
-            let expected = Factory.convert(simpleSchema)
+            const name = 'basic'
+            let expected = Factory.convert(basic, 'basic')
             expect(expected).to.be.an('object')
             expect(expected).to.have.property('schemas')
-            expect(expected.schemas).to.have.property('main')
-            expect(expected.schemas.main).to.not.have.property('$schema')
-
-            expected = Factory.convert(complexAllOfSchema)
-            expect(expected).to.be.an('object')
-            expect(expected).to.have.property('schemas')
-            expect(expected.schemas).to.have.property('main')
-            expect(expected.schemas.main).to.not.have.property('$schema')
-            expect(expected.schemas).to.have.property('message')
+            expect(expected.schemas).to.have.property('basic')
+            expect(expected.schemas.basic).to.not.have.property('$schema')
         });
     });
 });
