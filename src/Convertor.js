e1c1cd6c0f424758fe296595e514882cbdade84b
JaredAAT <jarede@gmail.com>
2022-12-02 19:38:53 +0000

    complete rewrite of the convertor

--- src/Convertor.js
+++ src/Convertor.js
@@ -4,20 +4,60 @@ const traverse = require('json-schema-traverse')
 const {v4: uuid} = require('uuid')
 
 class Convertor {
-    constructor(schema) {
+    constructor(schema = {}) {
         this.schema = JSON.parse(JSON.stringify(schema))
 
-        this.specialProperties = ['allOf', 'anyOf', 'items', 'oneOf', 'not', 'properties', 'additionalProperties',]
-        this.ofProperties = ['allOf', 'anyOf', 'oneOf',]
-        this.referencedSchemas = {}
-        this.bannedKeyWords = ['$schema', '$comment', '$id', 'version', 'examples', 'id',]
+        this.specialSchemaFields = [
+            'type',
+            'allOf',
+            'oneOf',
+            'anyOf',
+            'not',
+            'items',
+            'properties',
+            'additionalProperties',
+            'description',
+            'format',
+            'default',
+        ]
 
-        this.components = {
-            schemas: {}
-        }
+        let validSchemaFields = [
+            'title',
+            'multipleOf',
+            'maximum',
+            'exclusiveMaximum',
+            'minimum',
+            'exclusiveMinimum',
+            'maxLength',
+            'minLength',
+            'pattern',
+            'maxItems',
+            'minItems',
+            'uniqueItems',
+            'maxProperties',
+            'minProperties',
+            'required',
+            'enum',
+            '$ref'
+        ]
+
+        let openAPISchemaFields = [
+            'nullable',
+            'discriminator',
+            'readOnly',
+            'writeOnly',
+            'xml',
+            'externalDocs',
+            'example',
+            'deprecated',
+        ]
+
+        this.validSchemaFields = validSchemaFields.concat(this.specialSchemaFields, openAPISchemaFields)
+
+        this.components = {}
     }
 
-    convert() {
+    convert(name) {
         const traversal = (
             schema,
             jsonPointer,
@@ -27,197 +67,294 @@ class Convertor {
             parentSchema,
             property
         ) => {
-            const inJSONLink = new RegExp('#[\/a-zA-Z0-9]+', 'g')
-            const refExtraction = (properties) => {
-                const path = properties['$ref'].split('/')
-                const pathEnd = path[path.length-1]
-                const newPath = `#/components/schemas/${pathEnd}`
-                if (Object.keys(this.referencedSchemas).includes(newPath) === false) {
-                    Object.assign(this.referencedSchemas, {[newPath]: properties['$ref']})
-                    path.shift()
-                    const objPath = path.join('.')
-                    const newField = objPath.split('.').reduce((p,c)=>p&&p[c], rootSchema)
-                    Object.assign(this.components.schemas, {[pathEnd]: newField})
-                    properties['$ref'] = newPath
-                } else {
-                    properties['$ref'] = newPath
-                }
+            this.parseSchema(schema)
+            if (this.components.schemas) {
+                Object.assign(this.components.schemas, {[name]: rootSchema})
+            } else {
+                Object.assign(this.components, {schemas: {[name]: rootSchema}})
             }
 
-            const bannedWordsRemoval = () => {
-                if (Object.keys(schema).some(item => this.bannedKeyWords.includes(item))) {
-                    for (const bannedWord of this.bannedKeyWords) {
-                        delete schema[bannedWord]
-                    }
-                }
-            }
-
-            const convertNull = () => {
-                if (schema.type === 'null') {
-                    schema.nullable = true
-                    delete schema.type
-                }
-            }
-
-            const convertTypeArrays = () => {
-                if (Array.isArray(schema.type)) {
-                    const oneOf = []
-                    for (const type of schema.type) {
-                        const obj = {}
-                        if (type === 'null') {
-                            obj.nullable = true
-                        } else {
-                            obj.type = type
-                            if (schema.default) {
-                                obj.default = schema.default
-                                delete schema.default
-                            }
-
-                            for (const property of Object.keys(schema)) {
-                                if (type === 'array' && property === 'items') {
-                                    obj.items = schema[property]
-                                    delete schema.items
-                                }
-
-                                if (type === 'object' && property === 'properties') {
-                                    obj.properties = schema[property]
-                                    delete schema.properties
-                                }
-                            }
-                        }
-
-                        oneOf.push(obj)
-                    }
-                    schema.oneOf = oneOf
-                    delete schema.type
-                }
-            }
-
-            const convertItemArrays = () => {
-                if (Array.isArray(schema.items)) {
-                    const obj = {}
-                    for (const item of schema.items) {
-                        Object.assign(obj, item)
-                    }
-                    schema.items = obj
-                }
-            }
-
-            const convertDefaultValues = () => {
-                if (schema.type === 'boolean') {
-                    if (schema.default === 'true' || schema.default === 'false') {
-                        if (schema.default === 'true')
-                            schema.default = true
-                        else
-                            schema.default = false
-                    }
-                }
-
-                if (schema.type === 'number' || schema.type === 'integer') {
-                    if (typeof schema.default === 'string') {
-                        schema.default = parseInt(schema.default, 10) || 1
-                    }
-                }
-
-                if (schema.type === 'array' && schema.items === undefined) {
-                    schema.items = {nullable: true}
-                }
-
-                if (schema.type === 'string') {
-                    if (Object.keys(schema).indexOf('default') !== -1) {
-                        schema.default = `${schema.default}`
-                    }
-                }
-            }
-
-            const defaultValuesForOfProperties = () => {
-                if (schema.type === undefined && this.ofProperties.some(element => Object.keys(schema).includes(element))) {
-                    if (Object.keys(schema).includes('default')) {
-                        for (const property of this.ofProperties) {
-                            if (Object.keys(schema).includes(property)) {
-                                for (const ofProperty of schema[property]) {
-                                    if (ofProperty.type !== 'null' || ofProperty.nullable) {
-                                        ofProperty.default = schema.default
-                                        delete schema.default
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            convertNull()
-            convertTypeArrays()
-            convertItemArrays()
-            bannedWordsRemoval()
-            convertDefaultValues()
-            defaultValuesForOfProperties()
-
-            if (this.specialProperties.indexOf(parentKeyword) !== -1) {
-                if (this.ofProperties.indexOf(parentKeyword) !== -1) {
-                    for (const properties of parentSchema[parentKeyword]) {
-                        if (properties['$ref'] && inJSONLink.test(properties['$ref'])) {
-                            refExtraction(properties)
-                        } else {
-                            bannedWordsRemoval()
-                        }
-                    }
-                } else {
-                    if (
-                        schema['$ref'] &&
-                        inJSONLink.test(schema['$ref'])
-                    ) {
-                        refExtraction(schema)
-                    } else {
-                        bannedWordsRemoval()
-                    }
-                }
-            }
         }
 
         traverse(this.schema, traversal)
-
-        for (const [key, value] of Object.entries(this.referencedSchemas)) {
-            const path = value.split('/').slice(1)
-            const pathKey = path.pop()
-            delete path.reduce((previous, current) => previous[current], this.schema)[pathKey]
-        }
-
-        const removeDefinitions = (
-            schema,
-        ) => {
-            delete schema.definitions
-        }
-
-        traverse(this.schema, removeDefinitions)
-
-        this.removeEmpty(this.schema)
-
-        // force remove definitions
-        if (this.schema.definitions)
-            delete this.schema.definitions
-
-        if (Object.keys(this.components).includes('main') === false) {
-            Object.assign(this.components.schemas, {'main': this.schema})
-        } else {
-            Object.assign(this.components.schemas, {[`main-${uuid()}`]: this.schema})
-        }
-
         return this.components
     }
 
-    removeEmpty(schema) {
-        Object.keys(schema).forEach(key => {
-            if (schema[key]
-                && typeof schema[key] === 'object'
-                && this.removeEmpty(schema[key]) === null) {
+    parseSchema(schema) {
+        this.convertConst(schema)
+        this.convertArrays(schema)
+        this.convertIfThenElse(schema)
+        this.convertTypeArrays(schema)
+        this.convertDependencies(schema)
+        this.removeEmptyRequired(schema)
+        this.convertNullProperty(schema)
+        this.convertDefaultValues(schema)
+        this.removeInvalidFields(schema)
+    }
+
+    removeEmptyRequired(schema) {
+        if (schema.required && schema.required.length === 0)
+            delete schema.required
+    }
+
+    removeInvalidFields(schema) {
+        for (const key in schema) {
+            if (this.validSchemaFields.includes(key) === false) {
                 delete schema[key]
             }
-        })
+        }
+    }
 
-        if (Object.keys(schema).length === 0) {
-            return null
+    convertNullProperty(schema) {
+        if (schema?.type === 'null') {
+            schema.nullable = true
+            delete schema.type
+        }
+    }
+
+    convertTypeArrays(schema) {
+        if (Array.isArray(schema.type)) {
+            const oneOf = []
+            let defaultValue
+            let types = schema.type
+            let removeeNum = false
+            const nullable = types.includes('null')
+            if (nullable === true) {
+                types = types.filter(type => {
+                    if (type !== 'null')
+                        return type
+                })
+            }
+
+            for (const type of types) {
+                const newTypeObj = {}
+                if (defaultValue !== undefined && type === typeof defaultValue && (typeof defaultValue === type || defaultValue === false && type === 'boolean')) {
+                    newTypeObj.default = defaultValue
+                }
+
+                if (nullable === true) {
+                    newTypeObj.nullable = true
+                }
+
+                newTypeObj.type = type
+
+                if (schema.enum) {
+                    removeeNum = true
+                    const newEnum = []
+                     schema.enum.forEach(enumType => {
+                        if (type === typeof enumType)
+                            newEnum.push(enumType)
+                    })
+                    newTypeObj.enum = newEnum
+                }
+
+                if (Object.keys(schema).includes('default')) {
+                    defaultValue = schema.default
+                    delete schema.default
+                    if (type === typeof defaultValue || (typeof defaultValue === type || defaultValue === false && type === 'boolean'))
+                        newTypeObj.default = defaultValue
+
+                }
+
+                for (const property of Object.keys(schema)) {
+                    if (type === 'array' && property === 'items') {
+                        newTypeObj.items = schema[property]
+                        delete schema.items
+                    }
+
+                    if (type === 'object' && property === 'properties') {
+                        newTypeObj.properties = schema[property]
+                        delete schema.properties
+                    }
+                }
+
+
+                oneOf.push(newTypeObj)
+            }
+
+            schema.oneOf = oneOf
+            if (removeeNum)
+                delete schema.enum
+            delete schema.type
+        }
+    }
+
+    convertDefaultValues(schema) {
+        if (schema.type === 'object') {
+            if (typeof schema.default === 'string') {
+                const schemaDefault = {}
+                // is it a property?
+                if (Object.keys(schema.properties).includes(schema.default)) {
+                    const schemaType = schema.properties[schema.default].type
+                    switch (schemaType) {
+                        case 'string':
+                            Object.assign(schemaDefault, {[schema.default]: ''})
+                            break;
+                        case 'number':
+                        case 'integer':
+                            Object.assign(schemaDefault, {[schema.default]: 0})
+                            break;
+                        case 'array':
+                            Object.assign(schemaDefault, {[schema.default]: []})
+                            break;
+                        case 'object':
+                            Object.assign(schemaDefault, {[schema.default]: {}})
+                            break;
+                        case 'boolean':
+                            Object.assign(schemaDefault, {[schema.default]: true})
+                            break;
+                    }
+                    schema.default = schemaDefault
+                }
+            }
+        }
+
+        if (schema.type === 'array') {
+            if (schema.items === undefined) {
+                schema.items = {nullable: true}
+            }
+
+            if (schema.default && Array.isArray(schema.default) === false) {
+                schema.default = [schema.default]
+            }
+        }
+
+        if (schema.type === 'boolean') {
+            if (schema.default === 'true' || schema.default === 'false') {
+                if (schema.default === 'true')
+                    schema.default = true
+                else
+                    schema.default = false
+            }
+        }
+
+        if (schema.type === 'number' || schema.type === 'integer') {
+            if (typeof schema.default === 'string') {
+                schema.default = parseInt(schema.default, 10) || 1
+            }
+        }
+
+        if (schema.type === 'string') {
+            if (Object.keys(schema).indexOf('default') !== -1) {
+                schema.default = `${schema.default}`
+            }
+        }
+    }
+
+    convertArrays(schema) {
+        if (Array.isArray(schema.items)) {
+            const obj = {}
+            for (const item of schema.items) {
+                Object.assign(obj, item)
+            }
+            schema.items = obj
+        }
+    }
+
+    convertConst(schema) {
+        if (schema.const) {
+            schema.enum = [schema.const]
+            delete schema.const
+        }
+    }
+
+    convertIfThenElse(schema) {
+        if (schema?.if && (schema?.then || schema?.else)) {
+            const ifSchemaRefName = `if-${uuid()}`
+
+            const traversal = (
+                schema,
+                jsonPointer,
+                rootSchema,
+                parentJSONPointer,
+                parentKeyword,
+                parentSchema,
+                property
+            ) => {
+                this.parseSchema(schema)
+            }
+
+            traverse(schema.if, traversal)
+
+            if (this.components.schemas) {
+                Object.assign(this.components.schemas, {[ifSchemaRefName]: schema.if})
+            } else {
+                Object.assign(this.components, {schemas: {[ifSchemaRefName]: schema.if}})
+            }
+
+            if (schema?.then && schema?.else) {
+                let oneOf = []
+                if (schema.then) {
+                    oneOf.push({
+                        allOf: [
+                            {
+                                $ref: `#/components/schemas/${ifSchemaRefName}`,
+                            },
+                            schema.then
+                        ]
+                    })
+                }
+
+                if (schema.else) {
+                    oneOf.push({
+                        allOf: [
+                            {
+                                not: {
+                                    $ref: `#/components/schemas/${ifSchemaRefName}`
+                                },
+                            },
+                            schema.else
+                        ]
+                    })
+                }
+                schema.oneOf = oneOf
+            }
+        }
+
+        delete schema.if
+        delete schema.then
+        delete schema.else
+    }
+
+    convertDependencies(schema) {
+        if (schema.dependencies || schema.dependentSchemas || schema.dependentRequired) {
+            const allOf = []
+            const anyOf = []
+            if (schema.dependentSchemas)
+                schema.dependencies = JSON.parse(JSON.stringify(schema.dependentSchemas))
+            else if (schema.dependentRequired)
+                schema.dependencies = JSON.parse(JSON.stringify(schema.dependentRequired))
+
+            for (const key of Object.keys(schema.dependencies)) {
+                if (typeof schema.dependencies[key] === 'object' && Array.isArray(schema.dependencies[key]) === false) {
+                    const anyOf = []
+                    const notObj = {
+                        not: {
+                            required: [key]
+                        }
+                    }
+                    anyOf.push(notObj)
+
+                    const newDep = Object.assign({}, schema.dependencies[key])
+                    anyOf.push(newDep)
+                    allOf.push({anyOf})
+                } else {
+                    const notObj = {
+                        not: {
+                            required: [key]
+                        }
+                    }
+                    anyOf.push(notObj)
+                    const newReq = {
+                        required: [schema.dependencies[key]]
+                    }
+                    anyOf.push(newReq)
+                }
+            }
+            if (allOf.length)
+                schema.allOf = allOf
+            else
+                schema.anyOf = anyOf
         }
     }
 }
